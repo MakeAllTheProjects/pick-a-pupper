25c4a0335f6bc9b4b75a509048c4d8461da03636
Meghan Bomberger <meghanbomberger@gmail.com>
2019-04-24 21:16:27 -0600

    error message update

--- src/components/user/UserRatings.js
+++ src/components/user/UserRatings.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 // COMPONENTS
 import './User.css'
 import dogsIcon from '../images/dog.png'
+import embarassedDog from '../images/dogscared.jpg'
 
 // CONTEXT
 import { withAction } from '../../context/ActionProvider.js'
@@ -26,11 +27,19 @@ class UserRatings extends Component {
                     </Link>
                     <p className="logout" onClick={this.handleLogout}>Logout</p>
                 </div>
-                {JSON.parse(localStorage.getItem("ratings")).map((dog, i) => {
-                    return  <div key={i} className="rated-dog-card" style={{backgroundImage: `url(${dog.img})`}}>
-                                <p>You rated this {dog.breed} {dog.rating} out of 10!</p>
-                            </div>
-                })}
+                { localStorage.getItem("ratings")
+                    ?   JSON.parse(localStorage.getItem("ratings")).map((dog, i) => {
+                            return  <div key={i} className="rated-dog-card" style={{backgroundImage: `url(${dog.img})`}}>
+                                        <p>You rated this {dog.breed} {dog.rating} out of 10!</p>
+                                    </div>
+                        })
+                    :   <div className="no-dogs-container">
+                            <img className="hiding-dog" alt="dog hiding face" src={embarassedDog}/>
+                            <h1>You haven't rated any dogs yet :(</h1>
+                            <h2>Go back and click on the "View Dog" button to get started.</h2>
+                        </div>
+                }                  
+                
             </section>
         )
     }
