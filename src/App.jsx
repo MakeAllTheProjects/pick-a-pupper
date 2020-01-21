08e1c53ceae0ea2c17004ab41f1c5b0265628b52
Meghan Bomberger <meghanbomberger@gmail.com>
2020-01-20 20:35:32 -0700

    deploy update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Dogs from "./pages/dogs/Dogs"
 import Home from "./pages/home/Home"
 import NotFound from "./pages/not-found/NotFound"
 import User from "./pages/user/User"
+import GitHubIcon from "./images/github-icon.svg"
 
 export default function App () {
 	return (
@@ -17,6 +18,14 @@ export default function App () {
 				<User path="/user" />
 				<About path="/about" />
 			</Router>
+			<a href="https://github.com/MeghanBomberger/pick-a-pupper/">
+				<img
+					alt="github repo"
+					title="github repo"
+					className="github"
+					src={GitHubIcon}
+				/>
+			</a>
 		</div>
 	)
 }
